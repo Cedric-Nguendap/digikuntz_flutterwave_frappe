b8654cc10614bcb70e58cd9dc9e6602365e9bdfe
Cedric-Yaba-In <c.nguendap@yaba-in.com>
2026-05-10 08:02:16 +0100

    feat: add loader in generate payment link

--- frappe_digikuntz_flutterwave/public/js/sales_invoice.js
+++ frappe_digikuntz_flutterwave/public/js/sales_invoice.js
@@ -7,6 +7,8 @@ frappe.ui.form.on('Sales Invoice', {
 
                 frappe.call({
                     method: "frappe_digikuntz_flutterwave.api.payment.create_payment_link",
+                    freeze: true,
+                    freeze_message: __("Generating payment link..."),
                     args: {
                         sales_invoice: frm.doc.name
                     },
@@ -14,7 +16,20 @@ frappe.ui.form.on('Sales Invoice', {
 
                         if (r.message && r.message.payment_link) {
 
-                            window.open(r.message.payment_link, "_blank");
+                            // window.open(r.message.payment_link, "_blank");
+                            frappe.msgprint({
+                                title: __('Notification'),
+                                message: __('Lien de paiement créé <b/><br/>{0}', [r.message.payment_link]),
+                                primary_action: {
+                                'label': 'Copier le lien',
+                                action: function() {
+                                    navigator.clipboard.writeText(r.message.payment_link)
+                                        .then(function() {
+                                            frappe.show_alert({ message:__('Lien copié dans le press-papier'), indicator:'green' }, 5);
+                                        })
+                                    }
+                                }
+                            });
 
                         } else {
 
